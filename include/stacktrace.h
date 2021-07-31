ad212755836f5047489c22d7ed6f8e6fd518442b
FloweyTheFlower420 <guywithanaxe42@gmail.com>
2021-07-30 23:57:01 -0500

    made lib header-only

--- include/stacktrace.h
+++ include/stacktrace.h
@@ -1,6 +1,7 @@
 #ifndef __STACKTRACE_H__
 #define __STACKTRACE_H__
 #include <ios>
+#include <iomanip>
 #include <ostream>
 #include <stdexcept>
 #include <functional>
@@ -24,14 +25,26 @@
 
 namespace stacktrace
 {
-    namespace detail {
+    namespace detail 
+    {
         // ostream stuff
         inline int geti() {
             static int i = std::ios_base::xalloc();
             return i;
         }
 
-        void default_print(const entry& e, std::ostream& os);
+        template<typename T>
+        inline void print_hex(T t, std::ostream& os)
+        {
+            os << std::setw(sizeof(T) * 2) << std::setfill('0') << std::hex << t << std::setw(0) << std::setfill(' ') << std::dec;
+        }
+
+        inline void default_print(const entry& e, std::ostream& os)
+        {
+            os << "AT: [";
+            print_hex(e.address, os);
+            os << "] " << e.file << ':' << e.line << " (" << e.function << ')';
+        };
     }
 
     // generates a raw stacktrace
@@ -67,7 +80,6 @@ namespace stacktrace
         dump_stacktrace(st, detail::default_print, os);
     }
 
-
     class stack_aware_exception : std::runtime_error
     {
         symbol_stacktrace trace;
@@ -78,13 +90,27 @@ namespace stacktrace
         std::string long_msg;
 
     public:
-        stack_aware_exception(
+        inline stack_aware_exception(
             const char* what,
             int line,
             const char* file,
             const char* func,
             const char* long_func
-        );
+        ) : runtime_error(what), line(line), file(file), func(func), long_func(long_func)
+        {
+            long_msg += "MESSAGE: ";
+            long_msg += what;
+            long_msg += "\nAT FUNC: ";
+            long_msg += long_func;
+            long_msg += "\nAT FILE: ";
+            long_msg += file;
+            long_msg += ":";
+            long_msg += std::to_string(line);
+            long_msg += " (";
+            long_msg += func;
+            long_msg += ')';
+            trace = get_traced(stacktrace(100));
+        }
 
         inline const char* get_file() const { return file; }
         inline const char* get_func() const { return func; }
@@ -95,6 +121,32 @@ namespace stacktrace
         
         friend std::ostream& operator<<(std::ostream& os, const stack_aware_exception& dt);
     };
+
+    inline std::ostream& operator<<(std::ostream& os, const stack_aware_exception& e)
+    {
+        int state = os.iword(detail::geti());
+        switch (state)
+        {
+        case 0:
+            os << e.what();
+            break;
+        case 1:
+            os << e.get_long_msg();
+            break;
+        case 2:
+            auto flags = os.flags();
+            os.clear();
+
+            os << e.get_long_msg() << "\n\n";
+            os << "STACKTRACE:" << "\n";
+            dump_stacktrace(e.get_stacktrace(), os);
+
+            os.setf(flags);
+            break;
+        }
+
+        return os;
+    }
 
     inline std::ostream& shortexcept(std::ostream& os) { os.iword(detail::geti()) = 0; return os; }
     inline std::ostream& longexcept(std::ostream& os) { os.iword(detail::geti()) = 1; return os; }
